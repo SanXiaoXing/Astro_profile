3f806ff9dbebafe7b3c1a79971ac80c08964f190
SanXiaoXing <1137199378@qq.com>
2024-05-29 21:27:29 +0800

    refactor(init): init file

---
+++ src/components/sections/TimeZoneCardV2.tsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react'
+import moment from 'moment-timezone'
+
+const Timezone = ({ timezone }: any) => {
+  const [dateTime, setDateTime] = useState('')
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = moment().tz(timezone)
+      setDateTime(now.format('h:mm:ss'))
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [timezone])
+
+  return (
+    <div style={{justifyContent: 'center', alignItems: 'center'}}>
+      <p style={{ fontSize: '48px', textAlign: 'center' }}>{dateTime}</p>
+    </div>
+  )
+}
+
+export default Timezone
