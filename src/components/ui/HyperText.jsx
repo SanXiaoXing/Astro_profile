1237a7dbd27bcd2811da02854eda2f23c4aaeb98
XiaoXing San <xiaoxingsan@XiaoXingdeMac-mini.local>
2024-10-12 00:52:50 +0800

    feat: 新增动画效果

---
+++ src/components/ui/HyperText.jsx
@@ -0,0 +1,12 @@
+import HyperText from "@/components/ui/hyper-text";
+
+export function HyperTextDemo() {
+  return (
+    <HyperText
+      className="text-2xl font-bold text-black dark:text-white"
+      text="Hyper Text"
+    />
+  );
+}
+
+export default HyperTextDemo;
